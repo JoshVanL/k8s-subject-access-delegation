cdb106a8d14969ad6af133fc5c41e833d6bed6ef
JoshVanL <jv15626@my.bristol.ac.uk>
2018-02-13 14:46:39 +0000

    Moves stdout/err to command.go

--- pkg/end_to_end/command.go
+++ pkg/end_to_end/command.go
@@ -6,48 +6,98 @@ import (
 	"fmt"
 	"io"
 	"os/exec"
+	"sync"
 
 	"github.com/hashicorp/go-multierror"
 )
 
 type Command struct {
-	Stdin  io.WriteCloser
-	Stderr io.ReadCloser
-	Stdout io.ReadCloser
+	stdinWriter  io.WriteCloser
+	stderrReader io.ReadCloser
+	stdoutReader io.ReadCloser
 
-	ScanStdout *bufio.Scanner
-	ScanStderr *bufio.Scanner
+	scanStdout *bufio.Scanner
+	scanStderr *bufio.Scanner
 
 	name string
 	args []string
 	cmd  *exec.Cmd
+
+	stdoutBuffer *bytes.Buffer
+	stderrBuffer *bytes.Buffer
+	success      chan struct{}
 }
 
 func NewCommand(name string, args []string) (command *Command, err error) {
 	var result *multierror.Error
 
 	command = &Command{
-		name: name,
-		args: args,
-		cmd:  exec.Command(name, args...),
+		name:         name,
+		args:         args,
+		cmd:          exec.Command(name, args...),
+		stdoutBuffer: &bytes.Buffer{},
+		stderrBuffer: &bytes.Buffer{},
+		success:      make(chan struct{}),
 	}
 
-	if command.Stdin, err = command.cmd.StdinPipe(); err != nil {
+	if command.stdinWriter, err = command.cmd.StdinPipe(); err != nil {
 		result = multierror.Append(result, fmt.Errorf("unable to get command StdinPipe: %v", err))
 	}
-	if command.Stderr, err = command.cmd.StderrPipe(); err != nil {
+	if command.stderrReader, err = command.cmd.StderrPipe(); err != nil {
 		result = multierror.Append(result, fmt.Errorf("unable to get command StderrPipe: %v", err))
 	}
-	if command.Stdout, err = command.cmd.StdoutPipe(); err != nil {
+	if command.stdoutReader, err = command.cmd.StdoutPipe(); err != nil {
 		result = multierror.Append(result, fmt.Errorf("unable to get command StdoutPipe: %v", err))
 	}
 
-	command.ScanStdout = bufio.NewScanner(command.Stdout)
-	command.ScanStdout.Split(bufio.ScanWords)
-	command.ScanStderr = bufio.NewScanner(command.Stderr)
-	command.ScanStderr.Split(bufio.ScanWords)
+	command.scanStdout = bufio.NewScanner(command.stdoutReader)
+	command.scanStdout.Split(bufio.ScanWords)
+	command.scanStderr = bufio.NewScanner(command.stderrReader)
+	command.scanStderr.Split(bufio.ScanWords)
 
 	return command, result.ErrorOrNil()
+}
+
+func (c *Command) Run() error {
+	var result *multierror.Error
+	var stdoutError error
+	var stderrError error
+
+	var wg sync.WaitGroup
+
+	c.success = make(chan struct{})
+
+	if err := c.Start(); err != nil {
+		return err
+	}
+
+	wg.Add(2)
+
+	go func() {
+		if err := c.stdoutScan(); err != nil {
+			stdoutError = err
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		if err := c.stderrScan(); err != nil {
+			stderrError = err
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	result = multierror.Append(result, stdoutError, stderrError)
+
+	if err := c.Wait(); err != nil {
+		result = multierror.Append(result, err)
+	} else {
+		close(c.success)
+	}
+
+	return result.ErrorOrNil()
 }
 
 func (c *Command) Start() error {
@@ -58,28 +108,26 @@ func (c *Command) Wait() error {
 	return c.cmd.Wait()
 }
 
-func (c *Command) StdoutScan() (stdout string, err error) {
+func (c *Command) stdoutScan() error {
 	var result *multierror.Error
-	var buf bytes.Buffer
 
-	for c.ScanStdout.Scan() {
-		if _, err := buf.WriteString(c.ScanStdout.Text()); err != nil {
+	for c.scanStdout.Scan() {
+		if _, err := c.stdoutBuffer.WriteString(c.scanStdout.Text()); err != nil {
 			result = multierror.Append(result, err)
 		}
 	}
 
-	return buf.String(), result.ErrorOrNil()
+	return result.ErrorOrNil()
 }
 
-func (c *Command) StderrScan() (stderr string, err error) {
+func (c *Command) stderrScan() error {
 	var result *multierror.Error
-	var buf bytes.Buffer
 
-	for c.ScanStderr.Scan() {
-		if _, err := buf.WriteString(c.ScanStderr.Text()); err != nil {
+	for c.scanStderr.Scan() {
+		if _, err := c.stderrBuffer.WriteString(c.scanStderr.Text()); err != nil {
 			result = multierror.Append(result, err)
 		}
 	}
 
-	return buf.String(), result.ErrorOrNil()
+	return result.ErrorOrNil()
 }
--- pkg/end_to_end/test.go
+++ pkg/end_to_end/test.go
@@ -1,8 +1,8 @@
 package end_to_end
 
 import (
-	"bufio"
-	"fmt"
+	//"bufio"
+	//"fmt"
 
 	"github.com/sirupsen/logrus"
 )
@@ -13,29 +13,33 @@ func RunTests(log *logrus.Entry) error {
 		return fmt.Errorf("failed to create echo command: %v", err)
 	}
 
-	if err := firstTest.Start(); err != nil {
-		return fmt.Errorf("error starting echo command: %v", err)
+	if err := firstTest.Run(); err != nil {
+		return err
 	}
 
-	stdout, err := firstTest.StdoutScan()
-	if err != nil {
-		return fmt.Errorf("failed to get command stdout: %v", err)
-	}
+	//if err := firstTest.Start(); err != nil {
+	//	return fmt.Errorf("error starting echo command: %v", err)
+	//}
 
-	log.Infof("stdout: '%s'", stdout)
+	//stdout, err := firstTest.StdoutScan()
+	//if err != nil {
+	//	return fmt.Errorf("failed to get command stdout: %v", err)
+	//}
 
-	scanner := bufio.NewScanner(firstTest.Stdout)
-	scanner.Split(bufio.ScanWords)
-	for scanner.Scan() {
-		m := scanner.Text()
-		fmt.Printf(">>%s\n", m)
-	}
+	//log.Infof("stdout: '%s'", stdout)
 
-	if err := firstTest.Wait(); err != nil {
-		return fmt.Errorf("error waiting echo command: %v", err)
-	}
+	//scanner := bufio.NewScanner(firstTest.Stdout)
+	//scanner.Split(bufio.ScanWords)
+	//for scanner.Scan() {
+	//	m := scanner.Text()
+	//	fmt.Printf(">>%s\n", m)
+	//}
 
-	log.Infof("All command run successfully!")
+	//if err := firstTest.Wait(); err != nil {
+	//	return fmt.Errorf("error waiting echo command: %v", err)
+	//}
+
+	//log.Infof("All command run successfully!")
 
 	return nil
 }
