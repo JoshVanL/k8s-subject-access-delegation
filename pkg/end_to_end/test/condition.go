22fa2b3ea6596efadecb1fe50bfdae2f59b8b9b4
JoshVanL <jv15626@my.bristol.ac.uk>
2018-02-20 14:45:54 +0000

    Adds string condition

--- pkg/end_to_end/test/condition.go
+++ pkg/end_to_end/test/condition.go
@@ -5,6 +5,7 @@ import (
 )
 
 var _ Condition = &SplitStringCondition{}
+var _ Condition = &StringCondition{}
 
 type Condition interface {
 	TestConditon(stdout []string) (pass bool)
@@ -16,7 +17,16 @@ type SplitStringCondition struct {
 	match string
 }
 
+type StringCondition struct {
+	line  int
+	match string
+}
+
 func (s *SplitStringCondition) TestConditon(stdout []string) (pass bool) {
 	words := strings.Fields(stdout[s.line])
 	return words[s.split] == s.match
+}
+
+func (s *StringCondition) TestConditon(stdout []string) (pass bool) {
+	return stdout[s.line] == s.match
 }
