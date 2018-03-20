4bad385d815864726e64bfcedd7a95d4850af9b8
JoshVanL <jv15626@my.bristol.ac.uk>
2018-03-20 18:19:06 +0000

    Adds update pod trigger

---
+++ pkg/subject_access_delegation/trigger/pod/update.go
@@ -0,0 +1,128 @@
+package pod
+
+import (
+	"github.com/sirupsen/logrus"
+	informer "k8s.io/client-go/informers/core/v1"
+	"k8s.io/client-go/tools/cache"
+
+	authzv1alpha1 "github.com/joshvanl/k8s-subject-access-delegation/pkg/apis/authz/v1alpha1"
+	"github.com/joshvanl/k8s-subject-access-delegation/pkg/interfaces"
+	"github.com/joshvanl/k8s-subject-access-delegation/pkg/subject_access_delegation/utils"
+)
+
+const UpdatePodKind = "UpdatePodKind"
+
+type UpdatePod struct {
+	log *logrus.Entry
+
+	sad      interfaces.SubjectAccessDelegation
+	podName  string
+	replicas int
+
+	stopCh      chan struct{}
+	completedCh chan struct{}
+
+	count     int
+	completed bool
+	informer  informer.PodInformer
+}
+
+var _ interfaces.Trigger = &UpdatePod{}
+
+func NewUpdatePod(sad interfaces.SubjectAccessDelegation, trigger *authzv1alpha1.EventTrigger) (*UpdatePod, error) {
+	podTrigger := &UpdatePod{
+		log:         sad.Log(),
+		sad:         sad,
+		podName:     trigger.Value,
+		replicas:    trigger.Replicas,
+		stopCh:      make(chan struct{}),
+		completedCh: make(chan struct{}),
+		count:       0,
+		completed:   false,
+		informer:    sad.KubeInformerFactory().Core().V1().Pods(),
+	}
+
+	podTrigger.informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		UpdateFunc: podTrigger.updateFunc,
+	})
+
+	return podTrigger, nil
+}
+
+func (p *UpdatePod) updateFunc(oldObj, newObj interface{}) {
+
+	old, err := utils.GetPodObject(oldObj)
+	if err != nil {
+		p.log.Errorf("failed to get old updated pod object: %v", err)
+		return
+	}
+
+	new, err := utils.GetPodObject(newObj)
+	if err != nil {
+		p.log.Errorf("failed to get new updated pod object: %v", err)
+		return
+	}
+	if new == nil || old == nil {
+		p.log.Error("failed to get pod, received nil object")
+	}
+
+	if old.Name != p.podName || p.sad.DeletedUid(old.UID) {
+		return
+	}
+
+	p.sad.AddUid(new.UID)
+
+	p.log.Infof("A pod '%s' has been updated", new.Name)
+	p.count++
+	if p.count >= p.replicas {
+		p.log.Infof("Required replicas was met")
+		p.completed = true
+		close(p.completedCh)
+	}
+}
+
+func (p *UpdatePod) WaitOn() (forceClosed bool) {
+	p.log.Debug("Trigger waiting")
+
+	if p.watchChannels() {
+		p.log.Debug("Update Pod Trigger was force closed")
+		return true
+	}
+
+	p.log.Debug("Update Pod Trigger completed")
+	return false
+}
+
+func (p *UpdatePod) watchChannels() (forceClose bool) {
+	select {
+	case <-p.stopCh:
+		return true
+	case <-p.completedCh:
+		return false
+	}
+}
+
+func (p *UpdatePod) Activate() {
+	p.log.Debug("Update Pod Trigger Activated")
+
+	go p.informer.Informer().Run(make(chan struct{}))
+
+	return
+}
+
+func (p *UpdatePod) Completed() bool {
+	return p.completed
+}
+
+func (p *UpdatePod) Delete() error {
+	close(p.stopCh)
+	return nil
+}
+
+func (p *UpdatePod) Replicas() int {
+	return p.replicas
+}
+
+func (p *UpdatePod) Kind() string {
+	return UpdatePodKind
+}
