27079d6a54ab52729c4bfc04abb574b08b46764f
JoshVanL <jv15626@my.bristol.ac.uk>
2018-03-24 16:53:34 +0000

    Fixes origin service account tests

--- pkg/subject_access_delegation/origin_subject/service_account/service_account_test.go
+++ pkg/subject_access_delegation/origin_subject/service_account/service_account_test.go
@@ -2,19 +2,92 @@ package service_account
 
 import (
 	"errors"
-	//"reflect"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/joshvanl/k8s-subject-access-delegation/pkg/subject_access_delegation/mocks"
 )
 
 var (
-	noBindings        = &rbacv1.RoleBindingList{Items: nil}
-	noClusterBindings = &rbacv1.ClusterRoleBindingList{Items: nil}
+	noRoleBindings        = new(rbacv1.RoleBindingList)
+	noClusterRoleBindings = new(rbacv1.ClusterRoleBindingList)
+	someBindings          = []*rbacv1.RoleBinding{
+		&rbacv1.RoleBinding{
+			RoleRef: rbacv1.RoleRef{
+				Name: "a-name-1",
+				Kind: "ServiceAccount",
+			},
+			Subjects: []rbacv1.Subject{
+				rbacv1.Subject{
+					Kind: "ServiceAccount",
+					Name: "me",
+				},
+				rbacv1.Subject{
+					Kind: "ServiceAccount",
+					Name: "notme",
+				},
+			},
+		},
+		&rbacv1.RoleBinding{
+			RoleRef: rbacv1.RoleRef{
+				Name: "a-name-2",
+				Kind: "ServiceAccount",
+			},
+			Subjects: []rbacv1.Subject{
+				rbacv1.Subject{
+					Kind: "ServiceAccount",
+					Name: "notme",
+				},
+			},
+		},
+	}
+	someClusterBindings = []*rbacv1.ClusterRoleBinding{
+		&rbacv1.ClusterRoleBinding{
+			RoleRef: rbacv1.RoleRef{
+				Name: "cluster-name-1",
+				Kind: "ServiceAccount",
+			},
+			Subjects: []rbacv1.Subject{
+				rbacv1.Subject{
+					Kind: "ServiceAccount",
+					Name: "me",
+				},
+				rbacv1.Subject{
+					Kind: "ServiceAccount",
+					Name: "notme",
+				},
+			},
+		},
+		&rbacv1.ClusterRoleBinding{
+			RoleRef: rbacv1.RoleRef{
+				Name: "cluster-name-2",
+			},
+			Subjects: []rbacv1.Subject{
+				rbacv1.Subject{
+					Kind: "ServiceAccount",
+					Name: "notme",
+				},
+			},
+		},
+	}
+
+	bindingsList = &rbacv1.RoleBindingList{
+		Items: []rbacv1.RoleBinding{
+			*someBindings[0],
+			*someBindings[1],
+		},
+	}
+
+	clusterBindingList = &rbacv1.ClusterRoleBindingList{
+		Items: []rbacv1.ClusterRoleBinding{
+			*someClusterBindings[0],
+			*someClusterBindings[1],
+		},
+	}
 )
 
 type fakeServiceAccount struct {
@@ -26,12 +99,16 @@ type fakeServiceAccount struct {
 	fakeCore   *mocks.MockCoreV1Interface
 
 	fakeRoleBindingsInterface        *mocks.MockRoleBindingInterface
-	fakeServiceAccountInterface      *mocks.MockServiceAccountInterface
 	fakeClusterRoleBindingsInterface *mocks.MockClusterRoleBindingInterface
+	fakeServiceAccountInterface      *mocks.MockServiceAccountInterface
+
+	fakeBindingInformer        *mocks.MockRoleBindingInformer
+	fakeClusterBindingInformer *mocks.MockClusterRoleBindingInformer
+	fakeSharedIndexInformer    *mocks.MockSharedIndexInformer
 }
 
 func newFakeServiceAccount(t *testing.T) *fakeServiceAccount {
-	s := &fakeServiceAccount{
+	u := &fakeServiceAccount{
 		ctrl: gomock.NewController(t),
 		ServiceAccount: &ServiceAccount{
 			namespace: "fakeNamespace",
@@ -39,316 +116,197 @@ func newFakeServiceAccount(t *testing.T) *fakeServiceAccount {
 		},
 	}
 
-	s.fakeClient = mocks.NewMockInterface(s.ctrl)
-	s.fakeRbac = mocks.NewMockRbacV1Interface(s.ctrl)
-	s.fakeCore = mocks.NewMockCoreV1Interface(s.ctrl)
-	s.fakeRoleBindingsInterface = mocks.NewMockRoleBindingInterface(s.ctrl)
-	s.fakeClusterRoleBindingsInterface = mocks.NewMockClusterRoleBindingInterface(s.ctrl)
+	u.fakeClient = mocks.NewMockInterface(u.ctrl)
+	u.fakeRbac = mocks.NewMockRbacV1Interface(u.ctrl)
+	u.fakeRoleBindingsInterface = mocks.NewMockRoleBindingInterface(u.ctrl)
+	u.fakeClusterRoleBindingsInterface = mocks.NewMockClusterRoleBindingInterface(u.ctrl)
+	u.fakeCore = mocks.NewMockCoreV1Interface(u.ctrl)
+	u.fakeServiceAccountInterface = mocks.NewMockServiceAccountInterface(u.ctrl)
 
-	s.fakeServiceAccountInterface = mocks.NewMockServiceAccountInterface(s.ctrl)
-	s.ServiceAccount.client = s.fakeClient
+	u.fakeBindingInformer = mocks.NewMockRoleBindingInformer(u.ctrl)
+	u.fakeClusterBindingInformer = mocks.NewMockClusterRoleBindingInformer(u.ctrl)
+	u.fakeSharedIndexInformer = mocks.NewMockSharedIndexInformer(u.ctrl)
+	u.ServiceAccount.bindingInformer = u.fakeBindingInformer
+	u.ServiceAccount.clusterBindingInformer = u.fakeClusterBindingInformer
 
-	s.fakeClient.EXPECT().Core().AnyTimes().Return(s.fakeCore)
-	s.fakeClient.EXPECT().Rbac().AnyTimes().Return(s.fakeRbac)
-	s.fakeCore.EXPECT().ServiceAccounts(s.ServiceAccount.namespace).AnyTimes().Return(s.fakeServiceAccountInterface)
+	u.ServiceAccount.client = u.fakeClient
 
-	return s
+	noRoleBindings.Items = make([]rbacv1.RoleBinding, 0)
+	noClusterRoleBindings.Items = make([]rbacv1.ClusterRoleBinding, 0)
+
+	return u
+}
+
+func TestServiceAccount_ServiceAccountRefs_None(t *testing.T) {
+	u := newFakeServiceAccount(t)
+	defer u.ctrl.Finish()
+
+	refs, clusterRefs := u.RoleRefs()
+	if len(refs) != 0 {
+		t.Errorf("expected no refs, got=%+v", refs)
+	}
+
+	if len(clusterRefs) != 0 {
+		t.Errorf("expected no clouster refs, got=%+v", clusterRefs)
+	}
+
+}
+
+func TestServiceAccount_ServiceAccountRefs_Some(t *testing.T) {
+	u := newFakeServiceAccount(t)
+	defer u.ctrl.Finish()
+
+	u.ServiceAccount.bindings = someBindings
+	u.ServiceAccount.clusterBindings = someClusterBindings
+
+	refs, clusterRefs := u.RoleRefs()
+	if len(refs) != 2 {
+		t.Errorf("expected 2 refs, got=%+v", refs)
+	}
+
+	if len(clusterRefs) != 2 {
+		t.Errorf("expected 2 refs, got=%+v", clusterRefs)
+	}
+}
+
+func TestServiceAccount_RoleBindings_ErrorBinding(t *testing.T) {
+	u := newFakeServiceAccount(t)
+	defer u.ctrl.Finish()
+
+	options := metav1.ListOptions{}
+
+	u.fakeClient.EXPECT().Rbac().Times(1).Return(u.fakeRbac)
+	u.fakeRbac.EXPECT().RoleBindings(gomock.Any()).Return(u.fakeRoleBindingsInterface)
+	u.fakeRoleBindingsInterface.EXPECT().List(options).Times(1).Return(nil, errors.New("this is an error"))
+
+	if err := u.roleBindings(); err == nil {
+		t.Error("expected error, got=none")
+	}
+}
+
+func TestServiceAccount_RoleBindings_ErrorClusterBinding(t *testing.T) {
+	u := newFakeServiceAccount(t)
+	defer u.ctrl.Finish()
+
+	options := metav1.ListOptions{}
+
+	u.fakeClient.EXPECT().Rbac().Times(2).Return(u.fakeRbac)
+
+	u.fakeRbac.EXPECT().RoleBindings(gomock.Any()).Return(u.fakeRoleBindingsInterface)
+	u.fakeRoleBindingsInterface.EXPECT().List(options).Times(1).Return(nil, nil)
+
+	u.fakeRbac.EXPECT().ClusterRoleBindings().Return(u.fakeClusterRoleBindingsInterface)
+	u.fakeClusterRoleBindingsInterface.EXPECT().List(options).Times(1).Return(nil, errors.New("this is an error"))
+
+	if err := u.roleBindings(); err == nil {
+		t.Error("expected error, got=none")
+	}
+}
+
+func TestServiceAccount_RoleBindings_Success(t *testing.T) {
+	u := newFakeServiceAccount(t)
+	defer u.ctrl.Finish()
+
+	options := metav1.ListOptions{}
+
+	u.fakeClient.EXPECT().Rbac().Times(2).Return(u.fakeRbac)
+
+	u.fakeRbac.EXPECT().RoleBindings(gomock.Any()).Return(u.fakeRoleBindingsInterface)
+	u.fakeRoleBindingsInterface.EXPECT().List(options).Times(1).Return(bindingsList, nil)
+
+	u.fakeRbac.EXPECT().ClusterRoleBindings().Return(u.fakeClusterRoleBindingsInterface)
+	u.fakeClusterRoleBindingsInterface.EXPECT().List(options).Times(1).Return(clusterBindingList, nil)
+
+	if err := u.roleBindings(); err != nil {
+		t.Errorf("unexpected error: %v", err)
+		return
+	}
+
+	if len(u.bindings) != 1 || len(u.clusterBindings) != 1 {
+		t.Errorf("unexpected number of bindings and cluster Rolebindings, expected=1, got=%d, %d", len(u.bindings), len(u.clusterBindings))
+		return
+	}
+
+	if u.bindings[0].RoleRef.Name != "a-name-1" {
+		t.Errorf("unexpected rolebinding name, expected=a-name-1, got=%s", u.bindings[0].RoleRef.Name)
+	}
+
+	if u.clusterBindings[0].RoleRef.Name != "cluster-name-1" {
+		t.Errorf("unexpected rolebinding name, expected=a-name-1, got=%s", u.clusterBindings[0].RoleRef.Name)
+	}
+}
+
+func TestServiceAccount_ResolveDestination_Error(t *testing.T) {
+	u := newFakeServiceAccount(t)
+	defer u.ctrl.Finish()
+
+	options := metav1.GetOptions{}
+
+	u.fakeClient.EXPECT().Core().Times(1).Return(u.fakeCore)
+	u.fakeCore.EXPECT().ServiceAccounts("fakeNamespace").Times(1).Return(u.fakeServiceAccountInterface)
+	u.fakeServiceAccountInterface.EXPECT().Get("me", options).Times(1).Return(nil, errors.New("an error"))
+
+	if err := u.ResolveOrigin(); err == nil {
+		t.Errorf("expected error, got none")
+	}
 }
 
 func TestServiceAccount_ResolveDestination_Nil(t *testing.T) {
-	s := newFakeServiceAccount(t)
-	defer s.ctrl.Finish()
+	u := newFakeServiceAccount(t)
+	defer u.ctrl.Finish()
 
-	s.fakeServiceAccountInterface.EXPECT().Get(s.ServiceAccount.name, gomock.Any()).Times(1).Return(nil, nil)
+	options := metav1.GetOptions{}
 
-	if err := s.ResolveOrigin(); err == nil {
-		t.Error("expected error but got none - rolebindings is nil")
+	u.fakeClient.EXPECT().Core().Times(1).Return(u.fakeCore)
+	u.fakeCore.EXPECT().ServiceAccounts("fakeNamespace").Times(1).Return(u.fakeServiceAccountInterface)
+	u.fakeServiceAccountInterface.EXPECT().Get("me", options).Times(1).Return(nil, nil)
+
+	if err := u.ResolveOrigin(); err == nil {
+		t.Errorf("expected error, got none")
 	}
 }
 
-func TestServiceAccount_ResolveOrigin_Error(t *testing.T) {
-	s := newFakeServiceAccount(t)
-	defer s.ctrl.Finish()
+func TestServiceAccount_ResolveDestination_Success(t *testing.T) {
+	u := newFakeServiceAccount(t)
+	defer u.ctrl.Finish()
 
-	s.fakeServiceAccountInterface.EXPECT().Get(s.ServiceAccount.name, gomock.Any()).Times(1).Return(&corev1.ServiceAccount{}, errors.New("this is an error"))
+	options := metav1.GetOptions{}
+	listoptions := metav1.ListOptions{}
 
-	if err := s.ResolveOrigin(); err == nil {
-		t.Error("expected error but got none - returned error")
-	}
-}
+	aServiceAccount := new(corev1.ServiceAccount)
+	aServiceAccount.Name = "me"
+	aServiceAccount.Namespace = "fakeNamespace"
 
-func TestServiceAccount_ResolveOrigin_Successful(t *testing.T) {
-	s := newFakeServiceAccount(t)
-	defer s.ctrl.Finish()
+	u.fakeClient.EXPECT().Core().Times(1).Return(u.fakeCore)
+	u.fakeCore.EXPECT().ServiceAccounts("fakeNamespace").Times(1).Return(u.fakeServiceAccountInterface)
+	u.fakeServiceAccountInterface.EXPECT().Get("me", options).Times(1).Return(aServiceAccount, nil)
 
-	s.fakeRbac.EXPECT().RoleBindings(gomock.Any()).Return(s.fakeRoleBindingsInterface)
-	s.fakeRoleBindingsInterface.EXPECT().List(gomock.Any()).Return(noBindings, nil)
+	u.fakeClient.EXPECT().Rbac().Times(2).Return(u.fakeRbac)
 
-	s.fakeRbac.EXPECT().ClusterRoleBindings().Return(s.fakeClusterRoleBindingsInterface)
-	s.fakeClusterRoleBindingsInterface.EXPECT().List(gomock.Any()).Return(noClusterBindings, nil)
+	u.fakeRbac.EXPECT().RoleBindings(gomock.Any()).Return(u.fakeRoleBindingsInterface)
+	u.fakeRoleBindingsInterface.EXPECT().List(listoptions).Times(1).Return(bindingsList, nil)
 
-	if err := s.roleBindings(); err != nil {
+	u.fakeRbac.EXPECT().ClusterRoleBindings().Return(u.fakeClusterRoleBindingsInterface)
+	u.fakeClusterRoleBindingsInterface.EXPECT().List(listoptions).Times(1).Return(clusterBindingList, nil)
+
+	u.fakeBindingInformer.EXPECT().Informer().AnyTimes().Return(u.fakeSharedIndexInformer)
+	u.fakeClusterBindingInformer.EXPECT().Informer().AnyTimes().Return(u.fakeSharedIndexInformer)
+
+	u.fakeSharedIndexInformer.EXPECT().AddEventHandler(gomock.Any()).Times(2)
+	u.fakeSharedIndexInformer.EXPECT().Run(gomock.Any()).Times(1)
+
+	if err := u.ResolveOrigin(); err != nil {
 		t.Errorf("unexpected error: %v", err)
+		return
 	}
+
+	refs, clusterRefs := u.RoleRefs()
+	if len(refs) != 1 {
+		t.Errorf("expected 1 refs, got=%+v", refs)
+	}
+
+	if len(clusterRefs) != 1 {
+		t.Errorf("expected 1 refs, got=%+v", clusterRefs)
+	}
+
 }
-
-func TestServiceAccount_RoleRefs_Nil(t *testing.T) {
-	s := newFakeServiceAccount(t)
-	defer s.ctrl.Finish()
-
-	refs, err := s.RoleRefs()
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-	}
-	if refs != nil {
-		t.Errorf("expected nil, got=%v", refs)
-	}
-}
-
-func TestServiceAccount_ServiceAccountObject_Success(t *testing.T) {
-	s := newFakeServiceAccount(t)
-	defer s.ctrl.Finish()
-
-	//s.fakeClient.EXPECT().Rbac().Times(1).Return(s.fakeRbac)
-	//s.fakeRbac.EXPECT().RoleBindings(s.namespace).Times(1).Return(s.fakeRoleBindingsInterface)
-	s.fakeRoleBindingsInterface.EXPECT().List(gomock.Any()).Return(nil, errors.New("this is an error"))
-
-	_, err := s.RoleRefs()
-	if err == nil {
-		t.Error("expected error but got none - returned error")
-	}
-}
-
-//func TestServiceAccount_RoleRefs_Successful_None(t *testing.T) {
-//	s := newFakeServiceAccount(t)
-//	defer s.ctrl.Finish()
-//
-//	roleRefsReturn := &rbacv1.RoleBindingList{}
-//
-//	s.fakeClient.EXPECT().Rbac().Times(1).Return(s.fakeRbac)
-//	s.fakeRbac.EXPECT().RoleBindings(s.namespace).Times(1).Return(s.fakeRoleBindingsInterface)
-//	s.fakeRoleBindingsInterface.EXPECT().List(gomock.Any()).Return(roleRefsReturn, nil)
-//
-//	refs, err := s.RoleRefs()
-//	if err != nil {
-//		t.Errorf("unexpected error: %v", err)
-//	}
-//
-//	if len(refs) != 0 {
-//		t.Errorf("unexpected role refs exp=nothing got=%+v", refs)
-//	}
-//}
-//
-//func TestServiceAccount_RoleRefs_Successful_All(t *testing.T) {
-//	s := newFakeServiceAccount(t)
-//	defer s.ctrl.Finish()
-//
-//	roleBindingsReturn := &rbacv1.RoleBindingList{}
-//
-//	var roleRef1, roleRef2, roleRef3 rbacv1.RoleRef
-//	roleRef1.Name = "roleRef1"
-//	roleRef1.Kind = "Role"
-//	roleRef2.Name = "roleRef2"
-//	roleRef2.Kind = "Role"
-//	roleRef3.Name = "roleRef3"
-//	roleRef3.Kind = "Role"
-//
-//	subjects := []rbacv1.Subject{
-//		rbacv1.Subject{
-//			Kind: "Pod",
-//			Name: "foo",
-//		},
-//		rbacv1.Subject{
-//			Kind: "ServiceAccount",
-//			Name: "me",
-//		},
-//		rbacv1.Subject{
-//			Kind: "Pod",
-//			Name: "me",
-//		},
-//		rbacv1.Subject{
-//			Kind: "ServiceAccount",
-//			Name: "bar",
-//		},
-//	}
-//
-//	items := []rbacv1.RoleBinding{
-//		rbacv1.RoleBinding{
-//			RoleRef:  roleRef1,
-//			Subjects: subjects,
-//		},
-//		rbacv1.RoleBinding{
-//			RoleRef:  roleRef2,
-//			Subjects: subjects,
-//		},
-//		rbacv1.RoleBinding{
-//			RoleRef:  roleRef3,
-//			Subjects: subjects,
-//		},
-//	}
-//
-//	roleBindingsReturn.Items = items
-//
-//	roleRefsExp := []*rbacv1.RoleRef{
-//		&rbacv1.RoleRef{
-//			Name: "roleRef1",
-//			Kind: "Role",
-//		},
-//		&rbacv1.RoleRef{
-//			Name: "roleRef2",
-//			Kind: "Role",
-//		},
-//		&rbacv1.RoleRef{
-//			Name: "roleRef3",
-//			Kind: "Role",
-//		},
-//	}
-//
-//	s.fakeClient.EXPECT().Rbac().Times(1).Return(s.fakeRbac)
-//	s.fakeRbac.EXPECT().RoleBindings(s.namespace).Times(1).Return(s.fakeRoleBindingsInterface)
-//	s.fakeRoleBindingsInterface.EXPECT().List(gomock.Any()).Return(roleBindingsReturn, nil)
-//
-//	refs, err := s.RoleRefs()
-//	if err != nil {
-//		t.Errorf("unexpected error: %v", err)
-//	}
-//
-//	if !reflect.DeepEqual(refs, roleRefsExp) {
-//		t.Errorf("unexpected role refs\nexp=%+v\ngot=%+v", roleRefsExp, refs)
-//	}
-//}
-//
-//func TestServiceAccount_RoleRefs_Successful_Some(t *testing.T) {
-//	s := newFakeServiceAccount(t)
-//	defer s.ctrl.Finish()
-//
-//	roleBindingsReturn := &rbacv1.RoleBindingList{}
-//
-//	var roleRef1, roleRef2, roleRef3 rbacv1.RoleRef
-//	roleRef1.Name = "roleRef1"
-//	roleRef1.Kind = "Role"
-//	roleRef2.Name = "roleRef2"
-//	roleRef2.Kind = "Role"
-//	roleRef3.Name = "roleRef3"
-//	roleRef3.Kind = "Role"
-//
-//	subjects := []rbacv1.Subject{
-//		rbacv1.Subject{
-//			Kind: "Pod",
-//			Name: "foo",
-//		},
-//		rbacv1.Subject{
-//			Kind: "Pod",
-//			Name: "me",
-//		},
-//		rbacv1.Subject{
-//			Kind: "ServiceAccount",
-//			Name: "bar",
-//		},
-//	}
-//
-//	items := []rbacv1.RoleBinding{
-//		rbacv1.RoleBinding{
-//			RoleRef:  roleRef1,
-//			Subjects: subjects,
-//		},
-//		rbacv1.RoleBinding{
-//			RoleRef:  roleRef2,
-//			Subjects: subjects,
-//		},
-//	}
-//
-//		Name: "me",
-//	})
-//
-//	items = append(items, rbacv1.RoleBinding{
-//		RoleRef:  roleRef3,
-//		Subjects: subjects,
-//	})
-//
-//	roleBindingsReturn.Items = items
-//
-//	roleRefsExp := []*rbacv1.RoleRef{
-//		&rbacv1.RoleRef{
-//			Name: "roleRef3",
-//			Kind: "Role",
-//		},
-//	}
-//
-//	s.fakeClient.EXPECT().Rbac().Times(1).Return(s.fakeRbac)
-//	s.fakeRbac.EXPECT().RoleBindings(s.namespace).Times(1).Return(s.fakeRoleBindingsInterface)
-//	s.fakeRoleBindingsInterface.EXPECT().List(gomock.Any()).Return(roleBindingsReturn, nil)
-//
-//	refs, err := s.RoleRefs()
-//	if err != nil {
-//		t.Errorf("unexpected error: %v", err)
-//	}
-//
-//	if !reflect.DeepEqual(refs, roleRefsExp) {
-//		t.Errorf("unexpected role refs\nexp=%+v\ngot=%+v", roleRefsExp, refs)
-//	}
-//}
-//
-//func TestServiceAccount_RoleRefs_Successful_NoRef(t *testing.T) {
-//	s := newFakeServiceAccount(t)
-//	defer s.ctrl.Finish()
-//
-//	roleBindingsReturn := &rbacv1.RoleBindingList{}
-//
-//	var roleRef1, roleRef2, roleRef3 rbacv1.RoleRef
-//	roleRef1.Name = "roleRef1"
-//	roleRef1.Kind = "Role"
-//	roleRef2.Name = "roleRef2"
-//	roleRef2.Kind = "Role"
-//	roleRef3.Name = "roleRef3"
-//	roleRef3.Kind = "Role"
-//
-//	subjects := []rbacv1.Subject{
-//		rbacv1.Subject{
-//			Kind: "Pod",
-//			Name: "foo",
-//		},
-//		rbacv1.Subject{
-//			Kind: "Pod",
-//			Name: "me",
-//		},
-//		rbacv1.Subject{
-//			Kind: "ServiceAccount",
-//			Name: "bee",
-//		},
-//		rbacv1.Subject{
-//			Kind: "ServiceAccount",
-//			Name: "bar",
-//		},
-//	}
-//
-//	items := []rbacv1.RoleBinding{
-//		rbacv1.RoleBinding{
-//			RoleRef:  roleRef1,
-//			Subjects: subjects,
-//		},
-//		rbacv1.RoleBinding{
-//			RoleRef:  roleRef2,
-//			Subjects: subjects,
-//		},
-//		rbacv1.RoleBinding{
-//			RoleRef:  roleRef3,
-//			Subjects: subjects,
-//		},
-//	}
-//
-//	roleBindingsReturn.Items = items
-//
-//	s.fakeClient.EXPECT().Rbac().Times(1).Return(s.fakeRbac)
-//	s.fakeRbac.EXPECT().RoleBindings(s.namespace).Times(1).Return(s.fakeRoleBindingsInterface)
-//	s.fakeRoleBindingsInterface.EXPECT().List(gomock.Any()).Return(roleBindingsReturn, nil)
-//
-//	refs, err := s.RoleRefs()
-//	if err != nil {
-//		t.Errorf("unexpected error: %v", err)
-//	}
-//
-//	if len(refs) != 0 {
-//		t.Errorf("unexpected role refs exp=nothing got=%+v", refs)
-//	}
-//}
